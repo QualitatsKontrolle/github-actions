522d5a001006952b971384132d695ff0f60a99e9
QualitatsKontrolle <katua-wambua@protonmail.com>
2024-06-06 15:41:05 +0300

    create sample rust program

---
+++ rust/sample/target/debug/deps/sample-a557d57c35473c71.d
@@ -0,0 +1,5 @@
+/home/wambuajoe/.ssh/quality-control/github-actions/rust/sample/target/debug/deps/sample-a557d57c35473c71: src/main.rs
+
+/home/wambuajoe/.ssh/quality-control/github-actions/rust/sample/target/debug/deps/sample-a557d57c35473c71.d: src/main.rs
+
+src/main.rs:
---
+++ rust/sample/target/debug/sample.d
@@ -0,0 +1,1 @@
+/home/wambuajoe/.ssh/quality-control/github-actions/rust/sample/target/debug/sample: /home/wambuajoe/.ssh/quality-control/github-actions/rust/sample/src/main.rs
