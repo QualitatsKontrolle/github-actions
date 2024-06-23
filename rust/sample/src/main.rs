7134fa524fbac2c252760aa0593a59238a8c81ac
QualitatsKontrolle <katua-wambua@protonmail.com>
2024-06-23 12:27:32 +0300

    read user input & parse into i32 integer

--- rust/sample/src/main.rs
+++ rust/sample/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+	println!("Enter an integer:");
+
+	let mut usr_input = String::new();
+	io::stdin().read_line(&mut usr_input)
+			   .expect("Failed to read line");
+	let trimmed_input = usr_input.trim();
+	// parse input as i32
+	let number: i32 = match trimmed_input.parse() {
+		Ok(num) => num,
+		Err(_) => {
+			// error handling occurs here
+			println!("Invalid integer");
+			return;
+		}
+	};
+	println!("Number: {}", number);
 }
