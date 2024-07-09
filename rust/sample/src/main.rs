e9f584f8f4f090049d1683d651cb3ae4a3f592e9
QualitatsKontrolle <katua-wambua@protonmail.com>
2024-07-09 18:20:53 +0300

    fix rustfmt linter error(s)

--- rust/sample/src/main.rs
+++ rust/sample/src/main.rs
@@ -1,20 +1,21 @@
 use std::io;
 
 fn main() {
-	println!("Enter an integer:");
+    println!("Enter an integer:");
 
-	let mut usr_input = String::new();
-	io::stdin().read_line(&mut usr_input)
-			   .expect("Failed to read line");
-	let trimmed_input = usr_input.trim();
-	// parse input as i32
-	let number: i32 = match trimmed_input.parse() {
-		Ok(num) => num,
-		Err(_) => {
-			// error handling occurs here
-			println!("Invalid integer");
-			return;
-		}
-	};
-	println!("Number: {}", number);
+    let mut usr_input = String::new();
+    io::stdin()
+        .read_line(&mut usr_input)
+        .expect("Failed to read line");
+    let trimmed_input = usr_input.trim();
+    // parse input as i32
+    let number: i32 = match trimmed_input.parse() {
+        Ok(num) => num,
+        Err(_) => {
+            // error handling occurs here
+            println!("Invalid integer");
+            return;
+        }
+    };
+    println!("Number: {}", number);
 }
